8844b327499d54aeeff5bd12d5c5ae4857ccb424
oluquejs <luquedevjs@icloud.com>
2025-04-01 14:21:42 -0300

    feat(initial): initial setup

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ExchangeModule } from './exchange/exchange.module';
+
+
+@Module({
+  imports: [ExchangeModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
