ac2f4483fd5581541d39111c76db43dac68df87c
oluquejs <luquedevjs@icloud.com>
2025-04-01 16:27:37 -0300

    should be called getCurrency twice + readme

--- src/exchange/exchange.service.spec.ts
+++ src/exchange/exchange.service.spec.ts
@@ -1,16 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ExchangeService } from './exchange.service';
+import { CurrenciesService, ExchangeService } from './exchange.service';
 import { BadRequestException } from '@nestjs/common';
+
 
 describe('ExchangeService', () => {
   let service: ExchangeService;
+  let currenciesService: CurrenciesService
 
   beforeEach(async () => {
+    const currenciesServiceMock = {
+      getCurrency: jest.fn()
+    };
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ExchangeService],
+      providers: [
+        ExchangeService,
+        {
+          provide: CurrenciesService, useFactory: () => currenciesServiceMock
+        }
+      ],
     }).compile();
 
     service = module.get<ExchangeService>(ExchangeService);
+    currenciesService = module.get<CurrenciesService>(CurrenciesService);
   });
 
   it('should be defined', () => {
@@ -26,8 +37,14 @@ describe('ExchangeService', () => {
 
     it('should not throw if called with valid params', async () => {
       await expect(
-        service.convertAmount({from: 'USD', to:'BRL', amount: 1}),
+        service.convertAmount({from: 'USD', to: 'BRL', amount: 1}),
     ).resolves.not.toThrow();
     });
+
+    it('should be called getCurrency twice', async () => {
+      await service.convertAmount({from: 'USD', to: 'BRL', amount: 1}),
+      await expect(currenciesService.getCurrency).toHaveBeenCalledTimes(2);
+    });
+
   });
 });
