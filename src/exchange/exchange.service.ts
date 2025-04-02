8e0fabde5082b4e069dfbd2731d3b2cddeea3e72
oluquejs <luquedevjs@icloud.com>
2025-04-02 11:47:35 -0300

    should be return conversion value

--- src/exchange/exchange.service.ts
+++ src/exchange/exchange.service.ts
@@ -14,8 +14,10 @@ export class ExchangeService {
     }
     
     try {
-      const currencyFrom = await this.currenciesService.getCurrency('USD');
-      const currencyFrom2 = await this.currenciesService.getCurrency('BRL');
+      const currencyFrom = await this.currenciesService.getCurrency(from);
+      const currencyTo = await this.currenciesService.getCurrency(to);
+
+      return { amount: (currencyFrom.value / currencyTo.value) * amount }
     } catch (error) {
       throw new Error(error);
     }
